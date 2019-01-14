a1ee01e3ec8980e1cb76d5275de7585687b5c6ee
iashchuk <221box@list.ru>
2019-01-14 23:23:23 +0300

    Добавляет стили для компонента Feed

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -1,17 +1,22 @@
 // Core
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
+
+//Instruments
+import Styles from './styles.m.css';
 
 // Components
+import StatusBar from 'components/StatusBar';
 import Composer from 'components/Composer';
 import Post from 'components/Post';
 
 export default class Feed extends Component {
     render() {
         return (
-            <Fragment>
+            <section className = { Styles.feed }>
+                <StatusBar />
                 <Composer />
                 <Post />
-            </Fragment>
+            </section>
         );
     }
 }
