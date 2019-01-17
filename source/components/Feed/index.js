a1ab9bf59b69819000169be8415540e61ac6735f
iashchuk <221box@list.ru>
2019-01-18 01:47:18 +0300

    Переносит передачу данных выше по иерархии

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -3,29 +3,24 @@ import React, { Component } from 'react';
 
 //Instruments
 import Styles from './styles.m.css';
-import avatar from 'theme/assets/lisa';
 
 // Components
 import StatusBar from 'components/StatusBar';
 import Composer from 'components/Composer';
 import Post from 'components/Post';
 
-const options = {
-    avatar,
-    currentUserFirstName: 'Lisa',
-    currentUserLastName:  'Simpson',
-};
-
 export default class Feed extends Component {
     render() {
+        const { avatar, currentUserFirstName } = this.props;
+
         return (
             <section className = { Styles.feed }>
                 <StatusBar />
                 <Composer
-                    avatar = { options.avatar }
-                    currentUserFirstName = { options.currentUserFirstName }
+                    avatar = { avatar }
+                    currentUserFirstName = { currentUserFirstName }
                 />
-                <Post { ...options } />
+                <Post { ...this.props } />
             </section>
         );
     }
--- source/containers/App/index.js
+++ source/containers/App/index.js
@@ -2,12 +2,21 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 
+//Instruments
+import avatar from 'theme/assets/lisa';
+
 // Components
 import Feed from 'components/Feed';
+
+const options = {
+    avatar,
+    currentUserFirstName: 'Lisa',
+    currentUserLastName:  'Simpson',
+};
 
 @hot(module)
 export default class App extends Component {
     render() {
-        return <Feed />;
+        return <Feed { ...options } />;
     }
 }
