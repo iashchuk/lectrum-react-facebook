4692ffe78c5da70375ffdbd0b273526ce539b712
iashchuk <221box@list.ru>
2019-01-27 00:19:54 +0300

    Добавляет state компоненту Feed

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -8,14 +8,34 @@ import Styles from './styles.m.css';
 import StatusBar from 'components/StatusBar';
 import Composer from 'components/Composer';
 import Post from 'components/Post';
+import Spinner from 'components/Spinner';
 
 export default class Feed extends Component {
+    state = {
+        posts: [
+            { id: '123', comment: 'Hi there!', created: 1526825076849 },
+            { id: '456', comment: 'Good evening!', created: 1526325076849 },
+        ],
+    };
+
     render() {
+        const { posts } = this.state;
+
+        const postsJSX = posts.map((post) => {
+            return (
+                <Post
+                    key = { post.id }
+                    { ...post }
+                />
+            );
+        });
+
         return (
             <section className = { Styles.feed }>
+                <Spinner isSpinning />
                 <StatusBar />
                 <Composer />
-                <Post />
+                {postsJSX}
             </section>
         );
     }
