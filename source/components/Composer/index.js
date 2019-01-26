51d604480191252a1f2556bc4df67c6a47f292c0
iashchuk <221box@list.ru>
2019-01-27 02:19:41 +0300

    Добавляет метод публикации поста при нажатии Enter

--- source/components/Composer/index.js
+++ source/components/Composer/index.js
@@ -15,7 +15,9 @@ class Composer extends Component {
     constructor() {
         super();
         this._updateComment = this._updateComment.bind(this);
+        this._handleFormSubmit = this._handleFormSubmit.bind(this);
         this._submitComment = this._submitComment.bind(this);
+        this._submitOnEnter = this._submitOnEnter.bind(this);
     }
 
     state = {
@@ -28,8 +30,12 @@ class Composer extends Component {
         });
     }
 
-    _submitComment(event) {
+    _handleFormSubmit(event) {
         event.preventDefault();
+        this._submitComment();
+    }
+
+    _submitComment() {
         const { comment } = this.state;
 
         if (!comment) {
@@ -42,6 +48,14 @@ class Composer extends Component {
         });
     }
 
+    _submitOnEnter(event) {
+        const enterKey = event.key === 'Enter';
+        if (enterKey) {
+            event.preventDefault();
+            this._submitComment();
+        }
+    }
+
     render() {
         const { comment } = this.state;
         const { avatar, currentUserFirstName } = this.props;
@@ -49,11 +63,12 @@ class Composer extends Component {
         return (
             <section className = { Styles.composer }>
                 <img src = { avatar } />
-                <form onSubmit = { this._submitComment }>
+                <form onSubmit = { this._handleFormSubmit }>
                     <textarea
                         placeholder = { `What\'s on your mind, ${currentUserFirstName}` }
                         value = { comment }
                         onChange = { this._updateComment }
+                        onKeyPress = { this._submitOnEnter }
                     />
                     <input
                         type = 'submit'
