0b967df0d4d8cc9e346c0356829f150f7651e497
iashchuk <221box@list.ru>
2019-01-27 06:06:38 +0300

    Добавляет метод отображения подробностей лайков

--- source/components/Like/index.js
+++ source/components/Like/index.js
@@ -26,6 +26,26 @@ class Like extends Component {
         this._getLikedByMe = this._getLikedByMe.bind(this);
         this._getLikeStyles = this._getLikeStyles.bind(this);
         this._likePost = this._likePost.bind(this);
+        this._showLikers = this._showLikers.bind(this);
+        this._hideLikers = this._hideLikers.bind(this);
+        this._getLikersList = this._getLikersList.bind(this);
+        this._getLikesDescription = this._getLikesDescription.bind(this);
+    }
+
+    state = {
+        showLikers: false,
+    };
+
+    _showLikers() {
+        this.setState({
+            showLikers: true,
+        });
+    }
+
+    _hideLikers() {
+        this.setState({
+            showLikers: false,
+        });
     }
 
     _likePost() {
@@ -49,9 +69,36 @@ class Like extends Component {
         });
     }
 
+    _getLikersList() {
+        const { showLikers } = this.state;
+        const { likes } = this.props;
+
+        const likesJSX = likes.map(({ firstName, lastName, id }) => (
+            <li key = { id }>{`${firstName} ${lastName}`}</li>
+        ));
+
+        return likes.length && showLikers ? <ul>{likesJSX}</ul> : null;
+    }
+
+    _getLikesDescription() {
+        const { likes, currentUserFirstName, currentUserLastName } = this.props;
+        const likedByMe = this._getLikedByMe();
+
+        if (likes.length === 1 && likedByMe) {
+            return `${currentUserFirstName} ${currentUserLastName}`;
+        } else if (likes.length === 2 && likedByMe) {
+            return `You and ${likes.length - 1} other`;
+        } else if (likedByMe) {
+            return `You and ${likes.length - 1} others`;
+        }
+
+        return likes.length;
+    }
+
     render() {
-        console.log(this.props);
         const likeStyles = this._getLikeStyles();
+        const likersList = this._getLikersList();
+        const likesDescription = this._getLikesDescription();
 
         return (
             <section className = { Styles.like }>
@@ -60,6 +107,14 @@ class Like extends Component {
                     onClick = { this._likePost }>
                     Like
                 </span>
+                <div>
+                    {likersList}
+                    <span
+                        onMouseEnter = { this._showLikers }
+                        onMouseLeave = { this._hideLikers }>
+                        {likesDescription}
+                    </span>
+                </div>
             </section>
         );
     }
