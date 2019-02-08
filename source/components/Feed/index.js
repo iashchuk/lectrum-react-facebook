1a1272da387092b4f2acdd343731bee62eb21bd2
iashchuk <221box@list.ru>
2019-02-08 03:23:49 +0300

    Добавляет анимацию появления поста (CSSTransition)

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -1,6 +1,6 @@
 // Core
 import React, { Component } from 'react';
-import { Transition } from 'react-transition-group';
+import { Transition, CSSTransition, TransitionGroup } from 'react-transition-group';
 import { fromTo } from 'gsap';
 
 //Instruments
@@ -161,13 +161,21 @@ class Feed extends Component {
 
         const postsJSX = posts.map((post) => {
             return (
-                <Catcher key = { post.id }>
-                    <Post
-                        { ...post }
-                        _likePost = { this._likePost }
-                        _removePost = { this._removePost }
-                    />
-                </Catcher>
+                <CSSTransition
+                    classNames = {{
+                        enter:       Styles.postInStart,
+                        enterActive: Styles.postInEnd,
+                    }}
+                    key = { post.id }
+                    timeout = {{ enter: 500, exit: 400 }}>
+                    <Catcher>
+                        <Post
+                            { ...post }
+                            _likePost = { this._likePost }
+                            _removePost = { this._removePost }
+                        />
+                    </Catcher>
+                </CSSTransition>
             );
         });
 
@@ -183,7 +191,9 @@ class Feed extends Component {
                     <Composer _createPost = { this._createPost } />
                 </Transition>
                 <Postman />
-                {postsJSX}
+                <TransitionGroup>
+                    {postsJSX}
+                </TransitionGroup>
             </section>
         );
     }
