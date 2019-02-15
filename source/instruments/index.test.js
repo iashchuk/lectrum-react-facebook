2ebd8d466e81bb8ec2394c1343648f8d525e72c2
iashchuk <221box@list.ru>
2019-02-15 16:11:43 +0300

    Добавляет тесты для функции instruments: delay

--- source/instruments/index.test.js
+++ source/instruments/index.test.js
@@ -1,5 +1,7 @@
 // Core
-import { sum } from './';
+import { sum, delay } from './';
+
+jest.setTimeout(10000);
 
 describe('instruments: sum', () => {
     test('sum function should be a function', () => {
@@ -15,4 +17,18 @@ describe('instruments: sum', () => {
         expect(sum(15, 9)).toBe(24);
         expect(sum(3, 9)).toMatchSnapshot();
     });
+});
+
+describe('instruments: delay', () => {
+    test('delay function should return a resolved promise', async () => {
+        await expect(delay()).resolves.toBeUndefined();
+    });
+
+    test('delay function should return a resolved promise in seTimeout(2000)', async () => {
+        await expect(delay(2000)).resolves.toBeUndefined();
+    });
+
+    // test('delay function should return a resolved promise with text', async () => {
+    //     await expect(delay()).resolves.toBe('A resolve promise');
+    // });
 });
