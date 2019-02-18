a056c6a33de9d1ff92da86e5f6ab59fc67400e04
iashchuk <221box@list.ru>
2019-02-19 02:51:04 +0300

    Тестирует компонент Composer. События

--- source/components/Composer/Composer.test.js
+++ source/components/Composer/Composer.test.js
@@ -19,6 +19,9 @@ const updatedState = {
 
 const result = mount(<Composer { ...props } />);
 
+const _submitCommentSpy = jest.spyOn(result.instance(), '_submitComment');
+const _handleFormSubmitSpy = jest.spyOn(result.instance(), '_handleFormSubmit');
+
 describe('composer: component', () => {
     test('should have 1 <section> element', () => {
         expect(result.find('section')).toHaveLength(1);
@@ -63,5 +66,33 @@ describe('composer: state', () => {
 
         expect(result.state()).toEqual(initialState);
         expect(result.find('textarea').text()).toBe('');
+    });
+});
+
+describe('composer: events', () => {
+    test('should handle textarea "change" event', () => {
+        result.find('textarea').simulate('change', {
+            target: {
+                value: comment,
+            },
+        });
+
+        expect(result.find('textarea').text()).toBe(comment);
+        expect(result.state()).toEqual(updatedState);
+    });
+
+    test('should handle form "submit" event', () => {
+        result.find('form').simulate('submit');
+
+        expect(result.state()).toEqual(initialState);
+    });
+
+    test('_createPost prop should be invoked once after form submission', () => {
+        expect(props._createPost).toHaveBeenCalledTimes(1);
+    });
+
+    test('_submitComment and _handleFormSubmit class methods should be invoked once after form submission', () => {
+        expect(_submitCommentSpy).toHaveBeenCalledTimes(1);
+        expect(_handleFormSubmitSpy).toHaveBeenCalledTimes(1);
     });
 });
